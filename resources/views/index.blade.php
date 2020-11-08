1ab30e70464c76066b6c4e34c167925ee8e85f1c
AsmaChloe <asmachloe@gmail.com>
2020-11-08 23:45:54 +0100

    8/11/20
    acceuil + footer + menu

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    @include('menu')
+</head>
+<body>
+    <div class="container p-5 my-1 border">
+        <h1 class="display-1">Work in progress</h1>
+        Work in progress<br>Work in progress<br>Work in progress<br>Work in progress<br>Work in progress
+    </div>
+
+    @include('footer')
+</body>
+</html>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Hveorungr</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand -->
+  <div class="navbar-brand">Hveorungr</div>
+
+  <!-- Links -->
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+        </li>
+</ul>
+</nav>
+      
+
+
+      
+
+</body>
+</html>
