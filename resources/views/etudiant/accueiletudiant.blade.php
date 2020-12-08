718175825fd4e02e47792697f5e97bb5cf39e8a0
AsmaChloe <74020223+AsmaChloe@users.noreply.github.com>
2020-12-08 20:45:03 +0100

    Piti changement accueil etudiant

--- resources/views/etudiant/accueiletudiant.blade.php
+++ resources/views/etudiant/accueiletudiant.blade.php
@@ -14,38 +14,53 @@
 </div>
 
 <div class="container-fluid">
-<div class="row">
-	<table class="table table-striped table-bordered">
-		<tr>
-			<td>
-				<h2>Présentiel</h2>
-				<p>xxxxxxxxxxxxxxxx</p>
-				<p>
-					<a class="btn" href="etudiant/presentiel">Voir le présentiel »</a>
-				</p>							
-			</td>
-			<td><h2>Notes</h2>
-				<p>xxxxxxxxxxxxxxxx</p>
-				<p>
-					<a class="btn" href="etudiant/notes">Voir les notes »</a>
-				</p>
-			</td>
-		</tr>
-		<tr>
-			<td><h2>Groupes</h2>
-				<p>xxxxxxxxxxxxxxxx</p>
-				<p>
-					<a class="btn" href="etudiant/groupes">Voir les groupes »</a>
-				</p>
-			</td>
-			<td>
-				<h2>Epreuves</h2>
-				<p>xxxxxxxxxxxxxxxx</p>
-				<p>
-					<a class="btn" href="etudiant/epreuves">Voir les épreuves »</a>
-				</p>
-			</td>
-		</tr>
-	</table>
-</div></div>
+	<div class="row">
+		<div class="col-md-2">    
+		</div>
+		
+		<div class="col-md-8">    
+	
+		<table class="table table-striped table-bordered">
+			<tr>
+				<td>
+					<h2>Présentiel</h2>
+					<p>Consultez le présentiel de vos séances ici.</p>
+					<p>
+						<a class="btn" href="etudiant/presentiel">Voir le présentiel »</a>
+					</p>							
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<h2>Notes</h2>
+					<p>Consultez vos notes ici.</p>
+					<p>
+						<a class="btn" href="etudiant/notes">Voir les notes »</a>
+					</p>
+				</td>
+			</tr>
+			<tr>
+				<td><h2>Groupes</h2>
+					<p>Consultez vos groupes ici.</p>
+					<p>
+						<a class="btn" href="etudiant/groupes">Voir les groupes »</a>
+					</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<h2>Epreuves</h2>
+					<p>Consultez toutes vos épreuves ici.</p>
+					<p>	
+						<a class="btn" href="etudiant/epreuves">Voir les épreuves »</a>
+					</p>
+				</td>
+			</tr>
+		</table>
+		</div>
+	
+		<div class="col-md-2">    
+		</div>
+	</div>
+</div>
 @endsection
