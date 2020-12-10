88ef41c9b0080319644b0498df71b98b0db6a5ab
Debdeb24 <64196556+Debdeb24@users.noreply.github.com>
2020-12-10 14:41:31 +0100

    10/12
    Je dépose ça le temps que je règle mon pb

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,23 +6,11 @@
 <!--Header-->
 <div class="container-fluid bg-light">
   <div class="container pt-5 pb-4" >
-    <h1 class="display-1 text-center mb-4">Work in progress</h1>
+    <h1 class="display-1 text-center mb-4"> Bienvenue sur Hveorungr </h1>
 
-    <p class="lead text-center mb-4">Coucou toi !</p>
+    <p class="lead text-center mb-4">Gestion de cohorte de formation<br><br>Hveorungr est une plateforme réservée aux étudiants leur permettant de consulter leurs notes et présentiel. </p>
     <br>
   </div>
 </div>
 
-<div class="container-fluid my-1">
-  <div class="row">
-    <div class="col-md-2">	
-    		    
-    </div>
-    <div class="col-md-10">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, nisl id feugiat elementum, dui ex molestie nulla, eu scelerisque risus nulla et lacus. Duis vitae nisi volutpat, rutrum mi et, aliquam dolor. Etiam ac ex finibus leo ornare interdum in quis magna. Ut ut dignissim tellus. Donec vestibulum, augue a imperdiet porttitor, nunc diam bibendum lacus, eu rhoncus orci nisl sed dui. In ut nisi eu ligula tincidunt bibendum id in augue. Fusce placerat hendrerit purus. Nunc commodo nibh ornare mollis posuere. Nam vel tortor semper, auctor ex nec, consequat odio. Etiam venenatis a nulla ut sagittis. Nullam id rutrum ligula. Curabitur tempus sed massa eget fermentum.<br>
-       Nulla nec ligula felis. Duis a velit vel dolor suscipit hendrerit. Pellentesque ornare quam non blandit porttitor. Curabitur sagittis massa non ipsum tempor tristique.<br>
-          Vivamus ipsum diam, rhoncus vel porttitor et, bibendum eu libero. Nulla tincidunt augue eu orci scelerisque mattis. Duis pharetra erat at elit placerat, sit amet tristique neque facilisis. Vestibulum facilisis blandit ex. Integer ornare eros sodales arcu tempor varius. Sed egestas rhoncus iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus, magna quis luctus feugiat, nisi purus viverra sem, non dictum sapien augue a tellus. Mauris sed libero et ex aliquam vulputate. Integer mollis eros et risus interdum bibendum ut sed diam. Aenean dapibus, nunc sit amet finibus tristique, arcu tortor ornare lacus, sed placerat quam lorem ut turpis. Ut tincidunt erat quis mauris convallis iaculis. Mauris tortor risus, laoreet eu vulputate semper, posuere ut purus. Vivamus ut erat eget ex cursus sagittis. Phasellus luctus varius augue. Cras hendrerit efficitur turpis, sit amet blandit metus interdum et.</p>
-		</div>
-  </div>
-</div>
 @endsection
