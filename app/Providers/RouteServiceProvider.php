930e51eb15a8b8ff3a99a4e5e2b5315c4408c998
AsmaChloe <74020223+AsmaChloe@users.noreply.github.com>
2020-12-13 13:43:54 +0100

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    //public const HOME = '/dashboard';
+    public const HOME = '/dashboard';
     public const REGISTERED_REDIRECT = '/administrateur/utilisateurs';
 
     /**
